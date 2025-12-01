ef6dc7be90340cbc5c5b4b6e7d5b024e4b0795cb
jarothbs <jarothbs@gmail.com>
2025-11-30 22:42:36 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/",  // <--- ¡AGREGA ESTO! Sin esto, Coolify se pierde.
   server: {
     host: "::",
     port: 8080,
